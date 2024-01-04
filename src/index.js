c9cb42257ea803ebc79c148bb21ed167146bbcaa
Luz Hernandez <hernandezluz18@gmail.com>
2024-01-04 15:14:57 -0500

    updated the title

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <div className="App">
       <Typography component="h1" variant="h2">
-        To-do List
+        Todo List
       </Typography>
 
       <TodoForm
